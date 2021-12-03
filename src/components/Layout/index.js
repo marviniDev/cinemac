24300f7df54ae4da1868d422f39620a4fc37009f
marviniDev <69425271+marviniDev@users.noreply.github.com>
2021-12-03 17:00:50 -0300

    Ajustes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -6,20 +6,12 @@ import { Container, FlexContent, Main } from "./style"
 // import Loading from "../Loading";
 
 function Layout({ children }) {
-  const pathArray = window.location.pathname.split('/')
-  if (pathArray[2]) {
-    const str = pathArray[2]
-    var capitalized = str[0].toUpperCase() + str.substr(1)
-  }
-
-  const [refenceTitlePage, setRefenceTitlePage] = useState(capitalized ? capitalized : "Dashboard")
-
   return (
     <>
       <Container className="Layout">
-          <MenuTop title={`${refenceTitlePage}`} className="MenuTop" />
+          <MenuTop className="MenuTop" />
           <FlexContent>
-          <Menu {...{ setRefenceTitlePage }} className="Menu" />
+          <Menu className="Menu" />
           <Main className="MainPrincipal">
             <div style={{ minHeight: '100%', position: 'relative', overflow: 'hidden', flex: 'none', boxShadow: '0px 0px 30px 0px rgb(0 0 0 / 25%)', borderRadius: ' 0.3125em' }}>
               {children}
