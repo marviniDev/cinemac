6fed0f3c6c23bb733b608eaaf28e4ac266b6c3e8
marviniDev <69425271+marviniDev@users.noreply.github.com>
2021-12-16 15:33:59 -0300

    Revert "Update Api.js"
    This reverts commit 2224e1fe2b46762c13da4556708bb321e3bc91d0.

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //http://localhost:8080/api/ - url local
 
 var Api = axios.create({
-  baseURL: "http://2db3-168-121-25-140.ngrok.io/api/",
+  baseURL: "https://2db3-168-121-25-140.ngrok.io/api/",
 });
 
 export default Api;
