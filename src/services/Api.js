138a623606040597b3992fd197ef54356acd6651
marviniDev <69425271+marviniDev@users.noreply.github.com>
2021-12-16 15:20:20 -0300

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //http://localhost:8080/api/ - url local
 
 var Api = axios.create({
-  baseURL: "https://3747-189-44-221-242.ngrok.io/api/",
+  baseURL: "https://2db3-168-121-25-140.ngrok.io/api/",
 });
 
 export default Api;
